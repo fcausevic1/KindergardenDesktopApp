096982d6c7157082b00fe46f297cc67e8375576f
Faris135 <fcausevic1@etf.unsa.ba>
2020-07-11 16:22:53 +0200

    Main UI Created

--- src/ba/unsa/etf/rs/Controller.java
+++ src/ba/unsa/etf/rs/Controller.java
@@ -1,4 +1,55 @@
 package ba.unsa.etf.rs;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class Controller {
+    public TableView gradesTableView;
+    public ListView teachersListView;
+    public TextField teacherFirstNameField;
+    public TextField teacherLastNameField;
+    public TextField teacherTelephoneField;
+    public TextField teacherEmailField;
+    public TextField teacherBirthplaceField;
+    public TextField teacherAddressField;
+    public TextField teacherJMBGField;
+    public DatePicker teacherDateField;
+    public ChoiceBox teacherGenderChoiceBox;
+    public ListView childrenListView;
+    public TextField childFirstNameField;
+    public TextField childLastNameField;
+    public TextField childParentNameField;
+    public TextField childParentTelephoneField;
+    public DatePicker childDateField;
+    public TextField childAddressField;
+    public TextField childBirthplaceField;
+    public ChoiceBox childGenderField;
+    public TextField childJMBGField;
+
+    public void onAddGrade(ActionEvent actionEvent) {
+    }
+
+    public void onEditGrade(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteGrade(ActionEvent actionEvent) {
+    }
+
+    public void onAddTeacher(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteTeacher(ActionEvent actionEvent) {
+    }
+
+    public void onAddChild(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteChild(ActionEvent actionEvent) {
+    }
+
+    public void onParentDetails(ActionEvent actionEvent) {
+    }
+
+    public void onActivity(ActionEvent actionEvent) {
+    }
 }
--- src/ba/unsa/etf/rs/Main.java
+++ src/ba/unsa/etf/rs/Main.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Daycare manager");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(500);
+        primaryStage.show();
     }
 }
