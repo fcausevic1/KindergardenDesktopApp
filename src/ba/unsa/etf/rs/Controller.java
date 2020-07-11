dcdebf59e45ed5cbd1cf11da70d3d0afcb15bc08
Faris135 <fcausevic1@etf.unsa.ba>
2020-07-11 17:54:40 +0200

    Controller Initialize Added

--- src/ba/unsa/etf/rs/Controller.java
+++ src/ba/unsa/etf/rs/Controller.java
@@ -1,11 +1,22 @@
 package ba.unsa.etf.rs;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class Controller {
-    public TableView gradesTableView;
-    public ListView teachersListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    //Observables
+    private final ObservableList<Gender> genders = FXCollections.observableArrayList(Gender.values());
+    private final ObservableList<User> teachers = FXCollections.observableArrayList();
+    private final ObservableList<Child> children = FXCollections.observableArrayList();
+    private final ObservableList<Grade> grades = FXCollections.observableArrayList();
+    public TableView<Grade> gradesTableView;
     public TextField teacherFirstNameField;
     public TextField teacherLastNameField;
     public TextField teacherTelephoneField;
@@ -14,8 +25,8 @@ public class Controller {
     public TextField teacherAddressField;
     public TextField teacherJMBGField;
     public DatePicker teacherDateField;
-    public ChoiceBox teacherGenderChoiceBox;
-    public ListView childrenListView;
+    public TableColumn<Grade, String> gradeColumn;
+    public TableColumn<Grade, User> teacherColumn;
     public TextField childFirstNameField;
     public TextField childLastNameField;
     public TextField childParentNameField;
@@ -23,8 +34,45 @@ public class Controller {
     public DatePicker childDateField;
     public TextField childAddressField;
     public TextField childBirthplaceField;
-    public ChoiceBox childGenderField;
+    public TableColumn<Grade, String> childrenColumn;
     public TextField childJMBGField;
+    public ListView<User> teachersListView;
+    public ChoiceBox<Gender> teacherGenderChoiceBox;
+    public ListView<Child> childrenListView;
+    public ChoiceBox<Gender> childGenderChoiceBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //ChoiceBox initialization
+        teacherGenderChoiceBox.setItems(genders);
+        childGenderChoiceBox.setItems(genders);
+
+        //ListView initalization
+        teachersListView.setItems(teachers);
+        childrenListView.setItems(children);
+
+        //Table initialization
+        gradesTableView.setItems(grades);
+        gradeColumn.setCellValueFactory(grade -> grade.getValue().nameProperty());
+        teacherColumn.setCellValueFactory(grade -> grade.getValue().teacherProperty());
+        childrenColumn.setCellValueFactory(grade -> new SimpleStringProperty(String.valueOf(grade.getValue().getChildren().size())));
+
+        //Test items
+        User teacher1 = new User();
+        teacher1.setFirstName("Teacher");
+        teacher1.setLastName("One");
+        teachers.add(teacher1);
+        Child child1 = new Child();
+        child1.setFirstName("Child");
+        child1.setLastName("One");
+        children.add(child1);
+        Grade grade1 = new Grade();
+        grade1.setName("Grade One");
+        grade1.setTeacher(teacher1);
+        grade1.getChildren().add(child1);
+        grades.add(grade1);
+
+    }
 
     public void onAddGrade(ActionEvent actionEvent) {
     }
