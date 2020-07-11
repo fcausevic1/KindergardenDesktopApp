4012d67757c15bc14a103ba0ce15820e533a11af
Faris135 <fcausevic1@etf.unsa.ba>
2020-07-11 18:02:04 +0200

    Change Fields to Property

--- src/ba/unsa/etf/rs/Child.java
+++ src/ba/unsa/etf/rs/Child.java
@@ -1,38 +1,50 @@
 package ba.unsa.etf.rs;
 
+import javafx.beans.property.SimpleObjectProperty;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Child extends User {
-    private User firstParent, secondParent;
+    private final SimpleObjectProperty<User> firstParent, secondParent;
     private ArrayList<Activity> activities;
 
     public Child() {
         super();
         this.activities = new ArrayList<>();
+        this.firstParent = new SimpleObjectProperty<>();
+        this.secondParent = new SimpleObjectProperty<>();
     }
 
     public Child(int id, String firstName, String lastName, String address, String jmbg, String placeOfBirth, LocalDate dateOfBirth, Gender gender, User firstParent, User secondParent, ArrayList<Activity> activities) {
         super(id, firstName, lastName, address, jmbg, placeOfBirth, dateOfBirth, gender);
-        this.firstParent = firstParent;
-        this.secondParent = secondParent;
+        this.firstParent = new SimpleObjectProperty<>(firstParent);
+        this.secondParent = new SimpleObjectProperty<>(secondParent);
         this.activities = activities;
     }
 
     public User getFirstParent() {
+        return firstParent.get();
+    }
+
+    public SimpleObjectProperty<User> firstParentProperty() {
         return firstParent;
     }
 
     public void setFirstParent(User firstParent) {
-        this.firstParent = firstParent;
+        this.firstParent.set(firstParent);
     }
 
     public User getSecondParent() {
+        return secondParent.get();
+    }
+
+    public SimpleObjectProperty<User> secondParentProperty() {
         return secondParent;
     }
 
     public void setSecondParent(User secondParent) {
-        this.secondParent = secondParent;
+        this.secondParent.set(secondParent);
     }
 
     public ArrayList<Activity> getActivities() {
